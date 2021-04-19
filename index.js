6148d6f51045c3fafc7a193abe22f9cc254c6cc2
Scott Erwin <scottallenerwin@gmail.com>
2021-04-19 19:30:23 -0400

    cleared out the data from last weeks homework

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const inquirer = require("inquirer");
 // gets Specific functions {gernerateMarkdown} from the export information from generateMarkdown.js
 const { generateMarkdown } = require("./js/generateMarkdown.js");
 
-// 2nd way get all Information from the export of generateMarkdown.js
-// const generateMarkdownInfo = require('./js/generateMarkdown.js');
-
 function promptUser() {
   // Creates an array of questions for user input
   inquirer
@@ -20,55 +17,9 @@ function promptUser() {
         message: "What is your Project Title?",
       },
       {
-        type: "input",
-        name: "descriptionMotivation",
-        message: "What was your motivation?",
-      },
-      {
-        type: "input",
-        name: "descriptionBuild",
-        message: "Why did you build this project?",
-      },
-      {
-        type: "input",
-        name: "descriptionProblemSolve",
-        message: "What problem does it solve?",
-      },
-      {
-        type: "input",
-        name: "descriptionLearn",
-        message: "What did you learn?",
-      },
-      {
         type: "confirm",
         name: "tableOfContents",
         message: "What do u want a Table of Contents?",
-      },
-      {
-        type: "input",
-        name: "installProject",
-        message: "What are the steps required to install your project?",
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "Add screenshots Name that is in the images folder",
-      },
-      {
-        type: "list",
-        name: "usageImageType",
-        message: "Choose an image type",
-        choices: ["PNG", "JPG"],
-      },
-      {
-        type: "input",
-        name: "usageAltText",
-        message: "Screenshots Alt text",
-      },
-      {
-        type: "input",
-        name: "credits",
-        message: "List your collaborators",
       },
       {
         type: "list",
@@ -79,36 +30,6 @@ function promptUser() {
           "GNU General Public License v3.0",
           "GNU Affero General Public License v3.0",
         ],
-      },
-      {
-        type: "input",
-        name: "licensesYear",
-        message: "Type in the Current Year",
-      },
-      {
-        type: "input",
-        name: "licensesFullName",
-        message: "Type in your Full Name",
-      },
-      {
-        type: "input",
-        name: "features",
-        message: "List your Projects Features",
-      },
-      {
-        type: "input",
-        name: "tests",
-        message: "Provide examples on how to test run your project",
-      },
-      {
-        type: "input",
-        name: "questionsGitHub",
-        message: "Enter your GitHub username",
-      },
-      {
-        type: "input",
-        name: "questionsEmail",
-        message: "Enter your E-mail",
       },
     ])
     .then((data) => {
@@ -121,11 +42,6 @@ function promptUser() {
       fs.writeFile(`./markdown/${filename}`, generateMarkdown(data), (err) =>
         err ? console.log(err) : console.log("Success!")
       );
-
-      // 2nd way to get data
-      // fs.writeFile(filename, generateMarkdownInfo.generateMarkdown(data), (err) =>
-      // err ? console.log(err) : console.log('Success!')
-      // );
     });
 }
 
