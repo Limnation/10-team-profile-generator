0f20809bb146b03569604e5e45f89365ebab2c97
Scott Erwin <scottallenerwin@gmail.com>
2021-04-23 14:39:48 -0400

    added four arrays of objects for intern engi manager and roll

--- index2.js
+++ index2.js
@@ -8,6 +8,90 @@ const responses = [];
 // gets Specific functions {gernerateMarkdown} from the export information from generateMarkdown.js
 const { generateHtml, generateCards } = require("./js/generateMarkdown.js");
 
+const manager = [
+  {
+    type: "list",
+    name: "manager",
+    message: "manager",
+    choices: ["manager"],
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Enter Manager name.",
+  },
+  {
+    type: "input",
+    name: "employeeID",
+    message: "Enter the employee ID.",
+  },
+  {
+    type: "input",
+    name: "emailAddress",
+    message: "Enter email address.",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Enter office number.",
+  },
+];
+
+const engineer = [
+  {
+    type: "input",
+    name: "nameE",
+    message: "Enter Engineer name.",
+  },
+  {
+    type: "input",
+    name: "employeeID",
+    message: "Enter the employee ID.",
+  },
+  {
+    type: "input",
+    name: "emailAddress",
+    message: "Enter email address.",
+  },
+  {
+    type: "input",
+    name: "GitHub",
+    message: "Enter Engineer's GitHub.",
+  },
+];
+
+const intern = [
+  {
+    type: "input",
+    name: "nameI",
+    message: "Enter Intern name.",
+  },
+  {
+    type: "input",
+    name: "employeeID",
+    message: "Enter the employee ID.",
+  },
+  {
+    type: "input",
+    name: "emailAddress",
+    message: "Enter email address.",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "Enter school.",
+  },
+];
+
+const role = [
+  {
+    type: "list",
+    name: "role",
+    message: "Do you want to add more members to the team?",
+    choices: ["engineer", "intern", "finished"],
+  },
+];
+
 // Fucntion that Created my HTML page
 function makeFile(data) {
   console.log(data);
